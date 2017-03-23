a91e5552ecbbbb51cec2039079708e4b54278468
Patryk-Wojtkowiak <patryk.wojtkowiak.ti15@zs1-swarzedz.pl>
2017-03-23 13:56:39 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', function() {
+  var a = document.getElementsByClassName('in_A');
+  var a_low = false;
+  var a_high = false;
+  a[0].style.display = 'none';
+  a[1].style.display = 'block';
+  a[2].style.display = 'none';
+  console.log(a)});
